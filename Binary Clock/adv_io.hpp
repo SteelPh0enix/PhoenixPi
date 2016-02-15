c06a27d3a192bf81bb111b1ddcd50723453f4486
PhoenixPL512 <phoenixpl512@gmail.com>
2016-02-15 21:33:17 +0100

    commited binary clock

---
+++ Binary Clock/adv_io.hpp
@@ -0,0 +1,15 @@
+#ifndef ADV_IO_HPP
+#define ADV_IO_HPP
+#include <wiringPi.h>
+#include <cmath>
+#include <vector>
+#include <cstdint>
+
+class advIO {
+public:
+	static void pinsWrite(std::vector<uint8_t> pins, uint32_t value);
+	static uint32_t pinsRead(std::vector<uint8_t> pins);
+	static void pinsWrite(uint8_t* pins, uint8_t pinsnum, uint32_t value);
+	static uint32_t pinsRead(uint8_t* pins, uint8_t pinsnum);
+};
+#endif
---
+++ Binary Clock/binclock.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <cstdint>
+#include <wiringPi.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <ctime>
+#include "adv_io.hpp"
+
+int main()
+{
+	//init GPIO
+	const std::vector<uint8_t> pins[3] = {
+		{ 18, 15, 14, 13, 3, 2 }, //hours
+		{ 8, 25, 24, 23, 22, 27, 17 }, //mins
+		{ 12, 6, 5, 7, 11, 9, 10 } //secs
+	};
+	wiringPiSetupGpio();
+	
+	//init time/time struct
+	boost::posix_time::ptime now;
+	tm actTime;
+	
+	//main loop
+	while(true)
+	{
+		now = boost::posix_time::second_clock::local_time(); //get actual time
+		actTime = boost::posix_time::to_tm(now); //convert to tm
+		
+		advIO::pinsWrite(pins[0], static_cast<uint32_t>(actTime.tm_hour));
+		advIO::pinsWrite(pins[1], static_cast<uint32_t>(actTime.tm_min));
+		advIO::pinsWrite(pins[2], static_cast<uint32_t>(actTime.tm_sec));
+		
+		sleep(1);
+	}
+	return 0;
+}
